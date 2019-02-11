f10ad9cfa6b80a9de56cf8ef351f14f9dd476310
Kevin Wang <kwangsan@gmail.com>
2019-02-11 13:56:06 -0500

    Update Resolver Function - remove `Link` resolver - add `Mutation`/`post` resolver

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Mutation {
-  post(url: String!, description: String!): Link!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`;
-
 let links = [
   {
     id: "link-0",
@@ -24,21 +7,27 @@ let links = [
     description: "Fullstack tutorial for GraphQL"
   }
 ];
-
+let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links
   },
-  Link: {
-    id: parent => parent.id,
-    description: parent => parent.description,
-    url: parent => parent.url
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    }
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
