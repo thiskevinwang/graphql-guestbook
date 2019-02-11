c87ad0408e3f2c6def69e44b51160fe356d7d578
Kevin Wang <kwangsan@gmail.com>
2019-02-11 16:47:23 -0500

    Update index.js - remove `links` array - remove `idCount` var - update resolvers - import `{ prisma }` - update `server` with context

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
 const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
 
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  }
-];
-let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    }
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      });
     },
     updateLink: (parent, args) => {
       const link = links[args.id];
@@ -41,6 +33,7 @@ const resolvers = {
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: { prisma }
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
