63d5c8e08237548a47f994e43ba7c0068e12e330
Kevin Wang <kwangsan@gmail.com>
2019-02-11 13:08:00 -0500

    Extend schema definition - Update typeDefs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-// 1
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `;
 
