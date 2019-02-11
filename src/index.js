d4eb9353a9c8034f542a9909f1da7b1f5574e220
Kevin Wang <kwangsan@gmail.com>
2019-02-11 13:50:18 -0500

    Implement resolver functions

--- src/index.js
+++ src/index.js
@@ -13,14 +13,26 @@ type Link {
 }
 `;
 
-// 2
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url
   }
 };
 
-// 3
 const server = new GraphQLServer({
   typeDefs,
   resolvers
