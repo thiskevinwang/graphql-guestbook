84434d315b1133de91f9baf8be5a61d29de51d0e
Kevin Wang <kwangsan@gmail.com>
2019-02-11 22:04:56 -0500

    Implement resolvers in index.js - add imports - update `resolvers`

--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
 
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: (root, args, context, info) => {
-      return context.prisma.links();
-    }
-  },
-  Mutation: {
-    post: (root, args, context) => {
-      return context.prisma.createLink({
-        url: args.url,
-        description: args.description
-      });
-    },
-    updateLink: (parent, args) => {
-      const link = links[args.id];
-      link.description = args.description;
-      link.url = args.url;
-      link.id = link.id;
-      links[args.id] = link;
-      return link;
-    },
-    deleteLink: (parent, args) => {
-      const link = links[args.id];
-      links.pop(link);
-      return link;
-    }
-  }
+  Query,
+  Mutation,
+  User,
+  Link
 };
 
 const server = new GraphQLServer({
